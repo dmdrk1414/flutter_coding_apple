6b3609c6f097f6c0e4460a00a096569f13ed6290
dmdrk1414 <seungchan141414@gmail.com>
2025-04-19 11:53:59 +0900

    ADD 프로젝트 파일

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); // APP의 메인 페이지, 앱을 시작해주세요
+}
+
+// 메인 페이지
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});  // 기본적으로 들어가는 코드
+  @override // 기본적으로 들어가는 코드
+  Widget build(BuildContext context) { // 기본적으로 들어가는 코드
+
+    return  MaterialApp(
+      // home: Text('안녕'),
+      //home: Icon(Icons.shop), // 아이콘 위젯
+      home: Image.asset('../assets/test.png')
+    );
+  }
+}

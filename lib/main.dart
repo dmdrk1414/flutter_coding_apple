a43ec0ae73c958e5db939f5c982dab172b5951ea
dmdrk1414 <seungchan141414@gmail.com>
2025-04-19 15:11:43 +0900

    FEAT Flexible 사용하는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -44,46 +44,12 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue,
         title: Text('앱임'),
       ),
-      body: Center(  // 전체를 중앙에 정렬
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,  // 수직 중앙 정렬
-          crossAxisAlignment: CrossAxisAlignment.center,  // 수평 중앙 정렬
-          children: [
-            SizedBox(width: 10), // 텍스트와 아이콘 사이의 간격 설정
-
-            /* Text 꾸미는 법 */
-            Text(
-              '안녕하세요',
-              style: TextStyle(
-                color: Colors.red, // 텍스트 색상 설정
-                fontWeight: FontWeight.w600, // 텍스트 두께 설정
-                fontSize: 30, // 텍스트 크기 설정
-                // color: Color.fromRGBO(r, g, b, opacity) // RGB를 이용하여 색감 입히기
-              ),
-            ),
-
-            /* Icon 꾸미는 법 */
-            Icon(
-              Icons.favorite, // 사용할 아이콘 설정
-              color: Colors.blue, // 아이콘 색상 설정
-              size: 30, // 아이콘 크기 설정
-            ),
-
-            /* 버튼을 꾸미는 방법 */
-            TextButton(
-                onPressed: (){},
-                child: Text('글자')
-            ),
-            ElevatedButton(
-                onPressed: (){},
-                child: Text('글자')
-            ),
-            IconButton(
-                onPressed: (){},
-                icon: Icon(Icons.star),
-            )
-          ],
-        ),
+      body: Row(
+        children: [ // %으로 만들기
+          Flexible( flex: 5, child: Container(color: Colors.blue,), ),
+          Flexible( flex: 5, child: Container(color: Colors.red,), ),
+          Flexible( flex: 5, child: Container(color: Colors.green,), )
+        ],
       ),
 
       bottomNavigationBar: BottomNavigationBar(

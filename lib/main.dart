963914edc5a41c942a33ea528546561763496f05
dmdrk1414 <seungchan141414@gmail.com>
2025-04-19 13:16:41 +0900

    FEAT Flex와 같이 정렬하는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,11 @@ class MyApp extends StatelessWidget {
 
       home: Scaffold( // 상중하로 나누어 주는 위젯
         appBar: AppBar(),
-        body: Column( // 세로로 줄줄이 나오게 하는 방법
+        body: Row( // 가로로 줄줄이 나오게 하는 방법
+          // mainAxisAlignment: MainAxisAlignment.center,   // 정렬하는 방법 FLEX와 유사
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 가로축 정렬하는 방법
+          crossAxisAlignment: CrossAxisAlignment.center,    // 세로축 정렬하는 방법
+
           children: const [
             Icon(Icons.star),
             Icon(Icons.star),

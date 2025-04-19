ba27658c90ce1bd1af690ee0ce69c53073547316
dmdrk1414 <seungchan141414@gmail.com>
2025-04-19 13:35:51 +0900

    FEAT 숙제 간단한 화면 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 // 메인 페이지
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});  // 기본적으로 들어가는 코드
   @override // 기본적으로 들어가는 코드
   Widget build(BuildContext context) { // 기본적으로 들어가는 코드
@@ -20,7 +21,10 @@ class MyApp extends StatelessWidget {
       // )
 
       home: Scaffold( // 상중하로 나누어 주는 위젯
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('앱임'),
+          backgroundColor: Colors.blue,
+        ),
         body: Row( // 가로로 줄줄이 나오게 하는 방법
           // mainAxisAlignment: MainAxisAlignment.center,   // 정렬하는 방법 FLEX와 유사
           mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 가로축 정렬하는 방법
@@ -33,8 +37,21 @@ class MyApp extends StatelessWidget {
           ],
         ),
 
-        bottomNavigationBar: BottomAppBar(
-          child: Text('바텀 앱바의 글자쓰기'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.phone),
+              label: '전화',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: '메시지',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.lock),
+              label: '잠금',
+            ),
+          ],
         ),
       ),
     );
